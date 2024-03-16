6e46431bfa8b9c762fbec593b6bac3e7f487f753
Reydel Ocon <reydel321@gmail.com>
2024-03-16 13:11:31 +0800

    added poppins as default font

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="container flex h-dvh w-dvw flex-col items-center justify-center">
       hello world
       <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        ...
+        dsad
       </button>
     </div>
   );
